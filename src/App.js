f3d87869104004845783f71c9d54f7b800884b7b
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-05-07 16:15:15 -0700

    Initial implementation
    - Setup create react app environment variable to hide my access key for the Unsplash API
    - Setup initial state variables for loading and storing retrieved photos
    - Setup initial fetch, at least for first page of data
    - Setup initial return, which is just a grid of the pictures.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+import React, { useState, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import Photo from './Photo';
+//
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
-function App() {
-  return <h2>stock photos starter</h2>
+function App () {
+	const [ loading, setLoading ] = useState(false);
+	const [ photos, setPhotos ] = useState([]);
+
+	const fetchImages = async () => {
+		setLoading(true);
+		let url;
+		url = `${mainUrl}${clientID}`;
+		try {
+			const response = await fetch(url);
+			const data = await response.json();
+			console.log(data);
+			setPhotos(data);
+		} catch (error) {
+			setLoading(false);
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchImages();
+	}, []);
+
+	return (
+		<section className='section'>
+			<article className='photos photos-center'>
+				{photos.map((photo) => {
+					return (
+						<div key={photo.id} className='photo'>
+							<img src={photo.urls.small} alt={photo.alt_description} />
+						</div>
+					);
+				})}
+			</article>
+		</section>
+	);
 }
 
-export default App
+export default App;
