9e2cd6fb91a0527bc9361b4de4efc5ba3a3fb5d3
root <root@NFrag-Home-Desktop.localdomain>
2021-05-08 09:49:42 -0700

    Updates

---
+++ precache-manifest.ecf26185dc335e616536cd2d2c924baf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafdffa958c86f1927c4f52c420461de",
+    "url": "/React-Stock_Photos/index.html"
+  },
+  {
+    "revision": "9af846d05c681fa11bd0",
+    "url": "/React-Stock_Photos/static/css/main.a44d4aa5.chunk.css"
+  },
+  {
+    "revision": "833aaddd2aad21d82868",
+    "url": "/React-Stock_Photos/static/js/2.f9fac789.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Stock_Photos/static/js/2.f9fac789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af846d05c681fa11bd0",
+    "url": "/React-Stock_Photos/static/js/main.89b2b820.chunk.js"
+  },
+  {
+    "revision": "4b811c59e56da69de95e",
+    "url": "/React-Stock_Photos/static/js/runtime-main.f0d0ac3a.js"
+  }
+]);
