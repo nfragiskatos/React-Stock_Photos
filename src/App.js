d9fe46f2ef3dea8ef506ca273bc97539200b33df
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-05-08 06:47:49 -0700

    Loading new images for infinite scroll
    - Created new state variable to keep track of page for request to API
    - Changed the API url to include the page query
    - during setPhotos, make sure to append the new photos instead of replacing old photos.
    - Added page state variable to the dependency list of the useEffect that  fetches data from the API
    - Update the page state when scroll has reached bottom of the page.

--- src/App.js
+++ src/App.js
@@ -9,15 +9,19 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App () {
 	const [ loading, setLoading ] = useState(false);
 	const [ photos, setPhotos ] = useState([]);
+	const [ page, setPage ] = useState(1);
 
 	const fetchImages = async () => {
 		setLoading(true);
 		let url;
-		url = `${mainUrl}${clientID}`;
+		const urlPage = `&page=${page}`;
+		url = `${mainUrl}${clientID}${urlPage}`;
 		try {
 			const response = await fetch(url);
 			const data = await response.json();
-			setPhotos(data);
+			setPhotos((oldPhotos) => {
+				return [ ...oldPhotos, ...data ];
+			});
 			setLoading(false);
 		} catch (error) {
 			setLoading(false);
@@ -30,14 +34,17 @@ function App () {
 		console.log('hello');
 	};
 
-	useEffect(() => {
-		fetchImages();
-	}, []);
+	useEffect(
+		() => {
+			fetchImages();
+		},
+		[ page ]
+	);
 
 	useEffect(() => {
 		const event = window.addEventListener('scroll', () => {
 			if (!loading && window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
-				console.log('it worked');
+				setPage((oldPage) => oldPage + 1);
 			}
 		});
 		return () => window.removeEventListener('scroll', event);
